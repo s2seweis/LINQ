2600d076f3d12c5074f55a4fe1b809a588fec5c2
Sebastian Hans Weissenborn <weissenborn.sebastian@gmail.com>
2024-10-29 09:41:47 +0100

    Projektdateien hinzufügen.

---
+++ Linq/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            OddNumbers(numbers);
+
+            // Wait for the user to press Enter before closing
+            //Console.WriteLine("Press Enter to close...");
+            //Console.ReadLine();
+
+            Console.ReadKey();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Numbers:");
+
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+            Console.WriteLine(oddNumbers);
+
+            foreach(int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
